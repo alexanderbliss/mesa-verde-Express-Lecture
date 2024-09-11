11cd756c026bccb655664e112f4f8996d40336c3
Alex Bliss <alexander.blss@gmail.com>
2024-09-10 19:09:50 -0500

    end of express lecture

--- server/server.js
+++ server/server.js
@@ -30,12 +30,13 @@ console.log("is server running?");
 
 // CRUD - CREATE READ UPDATE DELETE
 
-let quoteList = [
-    { text: 'That\'s one small step for a man, a giant leap for mankind.', author: 'Neil Armstrong' },
-    { text: 'The more you read, the better you\'re going to become as a storyteller.', author: 'Stan Lee' },
-    { text: 'May the force be with you', author: 'Everyone in star wars at some point' }
-];
+// let quoteList = [
+//     { text: 'That\'s one small step for a man, a giant leap for mankind.', author: 'Neil Armstrong' },
+//     { text: 'The more you read, the better you\'re going to become as a storyteller.', author: 'Stan Lee' },
+//     { text: 'May the force be with you', author: 'Everyone in star wars at some point' }
+// ];
 
+let quoteList = require('./modules/quoteList')
 // when we visit http://localhost:5001/quotes
 //in our browser, express will call this function
 app.get('/quotes', function(req,res) {
@@ -43,8 +44,35 @@ app.get('/quotes', function(req,res) {
     //send back the item in res.send()
     //so we can use it in our browser
     res.send(quoteList)
+    //res.status(400).send('that request was not valid');
 })
 
 // all of these are the same - Route = Path = URL
 // app.get('/quotes', function(req,res) {
-//      ^ is the method         ^ is an object with information about the reqest
+//      ^ is the method         
+
+// app.get('/quotes', function(req,res) {
+   //                           ^ is an object with information about the reqest
+
+   //// app.get('/quotes', function(req,res) {
+//                                       ^
+//whatever we pass to `res.send()` is the response Body
+
+
+//status codes
+//  200 OK: everything about this request worked
+//  201 Created: The Server created some new Data for you.
+//  400 Bad Reqest: the client sent some bad data
+//  404 Not Found: That URL or endpoint is not available
+//  500 Server Errror: something bad happend on the server
+
+// by default res.send() uses a 200
+
+//API "Application Programing Interface"
+//a way for programs to communicate with each other. 
+//In our case were talking about a web server that can send data back and forth to a client
+
+//HTTP
+//HTTP is the speciification that describes everything we just talk about. 
+//So the fact that a reqest is made of a route and method, this is all descibed in HTTP Spec
+
